61d4ff658984262b319f89479bcf7c7d9e134e31
Daniel Polo <106583643+danielPoloWork@users.noreply.github.com>
2022-10-28 21:33:19 +0200

    Create SumServiceImpl.java

---
+++ src/main/java/services/impls/arithmeticOperations/SumServiceImpl.java
@@ -0,0 +1,21 @@
+package services.impls.arithmeticOperations;
+
+import DTOs.CurrencyDto;
+import DTOs.ExpressionDto;
+import services.SubtractionSumService;
+import static services.impls.currencies.PennyServiceImpl.*;
+
+public class SumServiceImpl implements SubtractionSumService {
+    @Override
+    public CurrencyDto calculate(ExpressionDto expressionDto) {
+        int penny = expressionDto.getCurrencyA().getPenny() + expressionDto.getCurrencyB().getPenny();
+        int shilling = expressionDto.getCurrencyA().getShilling() + expressionDto.getCurrencyB().getShilling();
+        int pound = expressionDto.getCurrencyA().getPound() + expressionDto.getCurrencyB().getPound();
+
+        penny += convertPoundAndShillingIntoPenny(pound, shilling);
+
+        return convertPennyIntoPoundAndShilling(pound, shilling, penny);
+    }
+
+
+}
