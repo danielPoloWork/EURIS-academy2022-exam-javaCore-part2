96c78f97d3b34ca5201bcc85537baaac664c4449
Daniel Polo <106583643+danielPoloWork@users.noreply.github.com>
2022-10-28 21:33:11 +0200

    Create PennyServiceImpl.java

---
+++ src/main/java/services/impls/currencies/PennyServiceImpl.java
@@ -0,0 +1,50 @@
+package services.impls.currencies;
+
+import DTOs.CurrencyDto;
+import services.CurrencyService;
+import utils.enums.PennyConversionRate;
+import static services.impls.currencies.PoundServiceImpl.*;
+import static services.impls.currencies.ShillingServiceImpl.*;
+
+public class PennyServiceImpl implements CurrencyService {
+    @Override
+    public Integer conversion(CurrencyDto currencyDto) {
+        return (currencyDto.getPound() * PennyConversionRate.POUND.getRate())
+                + (currencyDto.getShilling() * PennyConversionRate.SHILLING.getRate())
+                + (currencyDto.getPenny());
+    }
+
+    public static Integer convertPoundAndShillingIntoPenny(Integer pound, Integer shilling) {
+        return (pound * PennyConversionRate.POUND.getRate())
+                + (shilling * PennyConversionRate.SHILLING.getRate());
+    }
+
+    public static CurrencyDto convertPennyIntoPoundAndShilling(Integer pound, Integer shilling, Integer penny) {
+        pound = convertPennyIntoPound(penny);
+        penny -= subtractConvertedPound(penny);
+
+        shilling = convertPennyIntoShilling(penny);
+        penny -= subtractConvertedShilling(penny);
+
+        return CurrencyDto.builder()
+                .pound(pound)
+                .shilling(shilling)
+                .penny(penny)
+                .build();
+    }
+
+    public static CurrencyDto convertPennyIntoPoundAndShilling(Integer pound, Integer shilling, Integer penny, Integer rest) {
+        pound = convertPennyIntoPound(penny);
+        penny -= subtractConvertedPound(penny);
+
+        shilling = convertPennyIntoShilling(penny);
+        penny -= subtractConvertedShilling(penny);
+
+        return CurrencyDto.builder()
+                .pound(pound)
+                .shilling(shilling)
+                .penny(penny)
+                .rest(rest)
+                .build();
+    }
+}
