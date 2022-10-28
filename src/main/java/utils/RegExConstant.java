b28f880cd598d51d53f7cd1480659ee1f7cfb922
Daniel Polo <106583643+danielPoloWork@users.noreply.github.com>
2022-10-28 21:32:21 +0200

    Create RegExConstant.java

---
+++ src/main/java/utils/RegExConstant.java
@@ -0,0 +1,9 @@
+package utils;
+
+public interface RegExConstant {
+    String EXPRESSION = "(\\d+p\\s*\\d+s\\s*\\d+d)";
+    String OPERATOR = "(\\[(\\+||\\-||\\*||\\/)\\])";
+    String VALUE = "(\\d+)";
+    String REGEX = "^" + EXPRESSION + "\\s*" + OPERATOR + "\\s*(" + EXPRESSION + "||" + VALUE + ")*";
+    String CURRENCY = "(\\d+)p\\s*(\\d+)s\\s*(\\d+)d";
+}
