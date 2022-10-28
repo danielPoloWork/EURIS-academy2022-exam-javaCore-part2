2423ff5f38fb9ab138d3442aa4738e33301d9034
Daniel Polo <106583643+danielPoloWork@users.noreply.github.com>
2022-10-28 21:33:24 +0200

    Create MultiplicationServiceImpl.java

---
+++ src/main/java/services/impls/arithmeticOperations/MultiplicationServiceImpl.java
@@ -0,0 +1,21 @@
+package services.impls.arithmeticOperations;
+
+import DTOs.CurrencyDto;
+import DTOs.ExpressionDto;
+import services.DivisionMultiplicationService;
+
+import static services.impls.currencies.PennyServiceImpl.*;
+
+public class MultiplicationServiceImpl implements DivisionMultiplicationService {
+    @Override
+    public CurrencyDto calculate(ExpressionDto expressionDto) {
+        int penny = expressionDto.getCurrencyA().getPenny();
+        int shilling = expressionDto.getCurrencyA().getShilling();
+        int pound = expressionDto.getCurrencyA().getPound();
+
+        penny += convertPoundAndShillingIntoPenny(pound, shilling);
+        penny = penny * expressionDto.getValue();
+
+        return convertPennyIntoPoundAndShilling(pound, shilling, penny);
+    }
+}
